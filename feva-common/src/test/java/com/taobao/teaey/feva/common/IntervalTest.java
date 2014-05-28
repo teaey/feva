792785e207657637c6148e659ca395d9a350e918
teaey <masfay@163.com>
2014-05-28 23:08:13 +0800

    add unit test

--- feva-common/src/test/java/com/taobao/teaey/feva/common/IntervalTest.java
+++ feva-common/src/test/java/com/taobao/teaey/feva/common/IntervalTest.java
@@ -2,6 +2,8 @@ package com.taobao.teaey.feva.common;
 
 import junit.framework.TestCase;
 
+import java.util.concurrent.locks.LockSupport;
+
 /**
  * @author teaey masfay@163.com
  * @date 2014-05-23
@@ -9,39 +11,12 @@ import junit.framework.TestCase;
  */
 public class IntervalTest extends TestCase {
     public void test() {
-        int round = 10000000;
-        for (int i = 0; i < round; i++) {
-            ElapseCounter.p();
-            ElapseCounter.q();
-            ElapseCounter.pn();
-            ElapseCounter.qn();
-        }
-
-        long t = System.currentTimeMillis();
-
-        for (int i = 0; i < round; i++) {
-            long x = System.currentTimeMillis();
-            long y = System.currentTimeMillis();
-            long w = y - x;
-        }
-        long j = System.currentTimeMillis();
-        System.out.println("System.currentTimeMillis use " + (j - t) + "ms");
-
-        t = System.currentTimeMillis();
-
-        for (int i = 0; i < round; i++) {
-            ElapseCounter.p();
-            ElapseCounter.q();
-        }
-        j = System.currentTimeMillis();
-        System.out.println("pq use " + (j - t) + "ms");
-
-        t = System.currentTimeMillis();
-        for (int i = 0; i < round; i++) {
-            ElapseCounter.pn();
-            ElapseCounter.qn();
-        }
-        j = System.currentTimeMillis();
-        System.out.println("pnqn use " + (j - t) + "ms");
+        long i = Interval.interval(new Runnable() {
+            @Override
+            public void run() {
+                LockSupport.parkNanos(1000000);
+            }
+        }, 200);
+        System.out.println(i);
     }
 }
